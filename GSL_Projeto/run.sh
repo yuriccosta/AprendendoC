132b90dc1438cf5f17c66546fe5c3189cac15103
yuriccosta <yuricoutinhoc@gmail.com>
2023-11-19 23:01:52 -0300

    Corrigido comando

--- GSL_Projeto/run.sh
+++ GSL_Projeto/run.sh
@@ -1,5 +1,5 @@
 gcc -Wall -I/usr/local/include -c -o output/main.o main.c
 gcc -L/usr/local/lib -o output/main output/main.o -lgsl -lgslcblas -lm
 # Aparentemente pode ser substituido por:
-# gcc -o main main.c -lgsl -lgslcblas -lm
+# gcc -o output/main main.c -lgsl -lgslcblas -lm
 ./output/main
